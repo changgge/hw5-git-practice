664b3e55684285131f70170484ce5ba8bc748e4a
changgge <55868358+changgge@users.noreply.github.com>
2020-10-03 23:16:20 -0400

    Test Upload

---
+++ hw5_tests.py
@@ -0,0 +1,67 @@
+#Name: Chang Ge
+#Uniqname: changgge
+
+import unittest
+import hw5_cards as cards
+
+class TestCard(unittest.TestCase):
+
+    def test_construct_Card(self):
+        c1 = cards.Card(0, 2)
+        c2 = cards.Card(1, 1)
+
+        self.assertEqual(c1.suit, 0)
+        self.assertEqual(c1.suit_name, "Diamonds")
+        self.assertEqual(c1.rank, 2)
+        self.assertEqual(c1.rank_name, "2")
+
+        self.assertIsInstance(c1.suit, int)
+        self.assertIsInstance(c1.suit_name, str)
+        self.assertIsInstance(c1.rank, int)
+        self.assertIsInstance(c1.rank_name, str)
+
+        self.assertEqual(c2.suit, 1)
+        self.assertEqual(c2.suit_name, "Clubs")
+        self.assertEqual(c2.rank, 1)
+        self.assertEqual(c2.rank_name, "Ace")
+
+    def test_1(self):
+        c = cards.Card(0,12)
+        self.assertEqual(c.rank_name,'Queen')
+    
+    def test_2(self):
+        c = cards.Card(1,2)
+        self.assertEqual(c.suit_name, "Clubs")
+
+    def test_3(self):
+        c = cards.Card(3,13)
+        self.assertEqual(cards.Card.__str__(c),'King of Spades')
+
+    def test_4(self):
+        d = cards.Deck()
+        self.assertEqual(len(d.cards),52)
+    
+    def test_5(self):
+        d = cards.Deck()
+        self.assertTrue(cards.Deck.deal_card(d), cards.Card())
+    
+    def test_6(self):
+        d = cards.Deck()        
+        cards.Deck.deal_card(d)
+        self.assertEqual(len(d.cards),51)
+    
+    def test_7(self):
+        d = cards.Deck()        
+        cards.Deck.deal_card(d,-1)
+        cards.Deck.replace_card(d,cards.Card(3,14))
+        self.assertEqual(len(d.cards),52)
+    
+    def test_8(self):
+        d = cards.Deck()        
+        cards.Deck.deal_card(d,-1)
+        cards.Deck.replace_card(d,cards.Card(3,4))
+        self.assertEqual(len(d.cards),51)
+
+
+if __name__=="__main__":
+    unittest.main()
